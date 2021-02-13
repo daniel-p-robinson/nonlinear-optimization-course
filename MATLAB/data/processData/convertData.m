f0e7dbc28222cc8f6550fc3325a24a751bf973bf
Roth <rothdyt@gmail.com>
2021-02-13 11:31:24 -0500

    delete matlab

--- MATLAB/data/processData/convertData.m
+++
@@ -1,7 +0,0 @@
-[b, A] = libsvmread('bodyfat_scale.txt');
-A = full(A);
-save('../data/bodyfat.mat', 'A', 'b');
-
-[b, A] = libsvmread('diabetes.txt');
-A = full(A);
-save('../data/diabetes.mat', 'A', 'b');
